32f0672971a473160ea0398caf7a64550cfc3360
M Powers <mp@mpowers.info>
2021-03-14 04:34:03 -0400

    Add basic js to fetch Bitcoin price

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var data_url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+async function getData(url) {
+  const response = await fetch(url);
+
+  return response.json(); // get JSON from the response 
+}
